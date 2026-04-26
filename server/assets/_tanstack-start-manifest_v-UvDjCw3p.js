4a520d8fa45008fbd7b9f129e78b2ca3c7716c6f
Genuka Hettiarachchchi <genukavh@gmail.com>
2026-04-26 23:23:42 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-UvDjCw3p.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Files/Projects/Web Dev/shilpa-ar-main/shilpa-ar-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/ProjectShilpaAR/assets/index-DdHz5_Ae.js"] }, "/": { filePath: "C:/Files/Projects/Web Dev/shilpa-ar-main/shilpa-ar-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/ProjectShilpaAR/assets/index-DA-v5n0G.js"] } }, clientEntry: "/ProjectShilpaAR/assets/index-DdHz5_Ae.js" });
+export {
+  tsrStartManifest
+};
